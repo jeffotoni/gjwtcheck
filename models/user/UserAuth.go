ba007f688121a88789fc967ede8fb234f2556ad0
jeffeotoni <jeff.otoni@gmail.com>
2021-12-01 02:23:32 -0300

    correction

--- models/user/UserAuth.go
+++ models/user/UserAuth.go
@@ -5,8 +5,8 @@ package muser
 type UserAuth struct {
 	User     string `json:"user,omitempty"`
 	Key      string `json:"key,omitempty"`
-	Public       string `json:"public,omitempty"`
-	Secret       string `json:"secret,omitempty"`
+	Public   string `json:"public,omitempty"`
+	Secret   string `json:"secret,omitempty"`
 	Password string `json:"password,omitempty"`
 	Expires  string `json:"expires,omitempty"`
 	Typex    string `json:"typex,omitempty"`
