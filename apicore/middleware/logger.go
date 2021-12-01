072caeb0bc181b533dd25130c6b514a38ab9fa84
jeffeotoni <jeff.otoni@gmail.com>
2021-12-01 00:15:23 -0300

    color logge

--- apicore/middleware/logger.go
+++ apicore/middleware/logger.go
@@ -9,10 +9,9 @@ import (
 
 //Logger log
 func Logger(app *fiber.App) {
-	//app.Use(mw.Logger("${time} ${method} ${path} - ${ip} - ${status} - ${latency}\n"))
 	if os.Getenv("ENV_AMBI") != "PROD" {
 		app.Use(logger.New(logger.Config{
-			Format:     "${pid} ${time} ${method} ${path} - ${ip} - ${status} - ${latency}\n",
+			Format:     "\u001b[91m${pid}\u001b[0m ${time} \u001b[93m${method}\u001b[0m ${path} - ${ip} - \u001b[92m${status}\u001b[0m - \u001b[94m${latency}\u001b[0m\n",
 			TimeFormat: "02-Jan-2006 15:04:05",
 			Output:     os.Stdout,
 		}))
